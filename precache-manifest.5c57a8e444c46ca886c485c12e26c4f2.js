7bfbc3dfea9f88b18103a3c673beceb70dd288fb
aga <tgfc131@tgfc.tw>
2020-06-18 13:57:22 +0800

    first commit

---
+++ precache-manifest.5c57a8e444c46ca886c485c12e26c4f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44812c9d8123ffca0167432ba2bd8518",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08ed478492fea4f99c9a",
+    "url": "./static/css/main.55bfc5e8.chunk.css"
+  },
+  {
+    "revision": "4c679e70f2823053c7ca",
+    "url": "./static/js/2.21d7d3ea.chunk.js"
+  },
+  {
+    "revision": "1201c8656ff5734f581290e1df7d9b22",
+    "url": "./static/js/2.21d7d3ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ed478492fea4f99c9a",
+    "url": "./static/js/main.eff6ec47.chunk.js"
+  },
+  {
+    "revision": "8caa1d23531be6958717",
+    "url": "./static/js/runtime-main.d9a86656.js"
+  }
+]);
